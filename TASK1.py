7b14c2a7893810cc7a151841b3bbfab4eee67ea2
Monika Bisht <bishtmonika009@gmail.com>
2026-01-15 06:31:35 -0800

    Add files via upload

---
+++ TASK1.py
@@ -0,0 +1,6 @@
+num1=float(input("enter first number:"))
+num2=float(input("enter second number:"))
+print("Addition:",num1,"+",num2,"=",num1+num2)
+print("Multiplication:",num1,"*",num2,"=",num1*num2)
+print("Divison:",num1,"/",num2,"=",num1/num2)
+print("Subtraction:",num1,"-",num2,"=",num1-num2)
